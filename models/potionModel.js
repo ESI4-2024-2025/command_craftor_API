ecb9cf2e4bcb6c250250d9efbc7af563775ee7ba
Benjamin0173 <115452942+Benjamin0173@users.noreply.github.com>
2025-06-30 11:23:22 +0200

    feat: add 'id' field to potion schema in potionModel

--- models/potionModel.js
+++ models/potionModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const potionSchema = Schema({
+    id: { type: Number, required: true },
     identifier: { type: String, required: true },
     version: { type: String, required: true },
 });
