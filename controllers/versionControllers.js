0cb8912faac64349cccc1d25d404bf8ad8594895
Benjamin0173 <115452942+Benjamin0173@users.noreply.github.com>
2025-07-03 10:05:55 +0200

    fix: correct sorting field in getAllVersions function to 'value'

--- controllers/versionControllers.js
+++ controllers/versionControllers.js
@@ -3,7 +3,7 @@ const Version = require('../models/VersionsModel');
 // Récupérer toutes les versions
 exports.getAllVersions = async (req, res) => {
     try {
-        const versions = await Version.find().sort({ number: -1 });
+        const versions = await Version.find().sort({ value: -1 });
         res.status(200).json(versions);
     } catch (error) {
         res.status(500).json({ message: 'Erreur lors de la récupération des versions', error });
